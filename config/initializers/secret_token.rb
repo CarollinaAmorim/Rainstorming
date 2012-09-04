e305339143a0d7d06a06d20b2a212de99a07299b
Carollina Amorim <carollina_brasil@yahoo.it>
2012-09-04 17:24:14 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainstorming::Application.config.secret_token = 'a47b63939961b830495bd31f37f3af4333e02b98bd641e215529be997eff911d26308de6a093f58fe91fcea3f28f8322de322a49f197054f3d916dd37178fc14'
